6a64cfaf6ca5cfac2c5c7342223f8e1ea9e38645
IrinaKhokhlova13 <irina.khokhlova.50@gmail.com>
2023-12-10 12:06:47 +0300

    Проверка функции edit_date, которая возвращает дату в нужном формате

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from utils import filter_data_list, read_json, sort_list_by_date
+from utils import filter_data_list, read_json, sort_list_by_date, edit_date
 
 
-data_list = read_json()
-data_list = filter_data_list(data_list)
-print(sort_list_by_date(data_list))
+#data_list = read_json()
+#data_list = filter_data_list(data_list)
+#print(sort_list_by_date(data_list))
+
+print(edit_date("2019-07-03T18:35:29.512364"))
+
