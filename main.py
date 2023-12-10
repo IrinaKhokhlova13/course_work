d13f487dee895c840f62f3d2eb11aa990281b5b8
IrinaKhokhlova13 <irina.khokhlova.50@gmail.com>
2023-12-10 13:59:11 +0300

    Написан ког итогового вывода

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-from utils import filter_data_list, read_json, sort_list_by_date, edit_date, hide_account_number
+from utils import read_json, filter_data_list, sort_list_by_date, edit_date, hide_account_number
 
 
-#data_list = read_json()
-#data_list = filter_data_list(data_list)
-#print(sort_list_by_date(data_list))
+data_list = read_json()
+data_list = filter_data_list(data_list)
+data_list = sort_list_by_date(data_list)
 
-#print(edit_date("2019-07-03T18:35:29.512364"))
+for i in data_list:
+    #Вывод даты в правильно формате и типа операции
+    print(f"{edit_date(i['date'])} {i['description']}")
 
-print(hide_account_number("Счет 64686473678894779589"))
-print(hide_account_number("Maestro 1596837868705199"))
-print(hide_account_number(""))
+    #Вывод информации номера карты(если он есть), номера счета(если он есть) или Оплата наличными
+    if "from" in i and "to" in i:
+        print(f"{hide_account_number(i['from'])} -> {hide_account_number(i['to'])}")
+    elif "from" not in i and "to" in i:
+        print(f"{hide_account_number('')} -> {hide_account_number(i['to'])}")
+    elif "from" in i and "to" not in i:
+        print(f"{hide_account_number(i['from'])} -> {hide_account_number('')}")
+    else:
+        print(hide_account_number(""))
+
+    #Вывод суммы и валюты
+    print(f"{i['operationAmount']['amount']} {i['operationAmount']['currency']['name']}")
+    print ("\n")
+
