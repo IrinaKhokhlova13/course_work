52eab607a12c7bb4a3a12dec9f077ae2fd61e973
IrinaKhokhlova13 <irina.khokhlova.50@gmail.com>
2023-12-10 13:03:19 +0300

    Проверка работы измененной функции hide_account_number

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from utils import filter_data_list, read_json, sort_list_by_date, edit_date
+from utils import filter_data_list, read_json, sort_list_by_date, edit_date, hide_account_number
 
 
 #data_list = read_json()
 #data_list = filter_data_list(data_list)
 #print(sort_list_by_date(data_list))
 
-print(edit_date("2019-07-03T18:35:29.512364"))
+#print(edit_date("2019-07-03T18:35:29.512364"))
 
+print(hide_account_number("Счет 64686473678894779589"))
+print(hide_account_number("Maestro 1596837868705199"))
+print(hide_account_number(""))
