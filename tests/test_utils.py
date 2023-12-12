53d6b4a19ed18fb19016b015480a1f4a3728ae51
IrinaKhokhlova13 <irina.khokhlova.50@gmail.com>
2023-12-12 21:10:49 +0300

    Создала тестовую функцию test_sort_list_by_date

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -42,5 +42,8 @@ def test_filter_data_list(list):
     assert utils.filter_data_list([]) == []
     assert utils.filter_data_list(list) == [{"id": 863064926, "state": "EXECUTED", "date": "2019-12-08T22:46:21.935582", "operationAmount": {"amount": "41096.24", "currency": {"name": "USD", "code": "USD"}}, "description": "Открытие вклада", "to": "Счет 90424923579946435907"}]
 
+def test_sort_list_by_date():
+    assert utils.sort_list_by_date([]) == []
+    assert utils.sort_list_by_date(data_list) == [{"date": "2019-12-08T22:46:21.935582"}, {"date": "2018-09-12T21:27:25.241689"}]
 
 
